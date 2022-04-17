6e277b19b17ae0712d2562b769f13898011d2020
thief <zx_Liu_uestc@163.com>
2022-04-17 09:41:40 +0800

    Update sum.js

--- api/sum.js
+++ api/sum.js
@@ -1,8 +1,11 @@
 module.exports = (req, res) => {
   const { a = 1, b = 2 } = req.query
   try {
+    if (isNaN(a) || isNaN(b)) {
+      throw ('a or b must be number')
+    }
     res.send(Number(a) + Number(b))
-  } catch {
-    res.send('a or b must be number')
+  } catch (err) {
+    res.send(err)
   }
 }
